e66685ecbf8c17850c304aa077d9e89bac77b98c
Murilod <murilo.dircksen@gmail.com>
2022-12-11 11:02:53 -0300

    inicio calculadora react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let resultado=10;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+    <tr>
+      <td colspan="4"><button id="result">{resultado}</button></td>
+    </tr>
+    <tr>
+      <td><button onClick={()=>{console.log(1)}} id = 'b1'>1</button></td>
+      <td><button id = 'b2'>2</button></td>
+      <td><button id = 'b3'>3</button></td>
+      <td><button id = 'bsum'>+</button></td>
+    </tr>
+    <tr>
+      <td><button id = 'b4'>4</button></td>
+      <td><button id = 'b5'>5</button></td>
+      <td><button id = 'b6'>6</button></td>
+      <td><button id = 'bsub'>-</button></td>
+    </tr>
+    <tr>
+      <td><button id = 'b7'>7</button></td>
+      <td><button id = 'b8'>8</button></td>
+      <td><button id = 'b9'>9</button></td>
+      <td><button id = 'bplus'>*</button></td>
+    </tr>
+    <tr>
+      <td  id = 'b0'><button>0</button></td>
+      <td  id = 'bdiv'><button>/</button></td>          
+    </tr>
+    <tr>
+      <td ><button class="ac" id = 'bclean'>AC</button></td>
+      <td><button id = 'bcoma'>.</button></td>
+      <td><button id= 'bequal'>=</button></td>          
+    </tr>
+  </table>
   );
 }
 
