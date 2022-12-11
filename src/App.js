bd59baa4ee18ddf4782e9d52e8b0b19d9b38b7fc
Murilod <murilo.dircksen@gmail.com>
2022-12-11 11:10:48 -0300

    adição console log teste

--- src/App.js
+++ src/App.js
@@ -3,37 +3,40 @@ import './App.css';
 
 function App() {
   let resultado=10;
+  function consolaValor(valor){
+    console.log(valor)
+  }
   return (
     <table>
     <tr>
-      <td colspan="4"><button id="result">{resultado}</button></td>
+      <td ><button id="result">{resultado}</button></td>
     </tr>
     <tr>
-      <td><button onClick={()=>{console.log(1)}} id = 'b1'>1</button></td>
-      <td><button id = 'b2'>2</button></td>
-      <td><button id = 'b3'>3</button></td>
-      <td><button id = 'bsum'>+</button></td>
+      <td><button onClick={()=>{console.log(1)}} >1</button></td>
+      <td><button onClick={()=>{console.log(2)}} >2</button></td>
+      <td><button onClick={()=>{console.log(3)}} >3</button></td>
+      <td><button onClick={()=>{console.log('+')}} >+</button></td>
     </tr>
     <tr>
-      <td><button id = 'b4'>4</button></td>
-      <td><button id = 'b5'>5</button></td>
-      <td><button id = 'b6'>6</button></td>
-      <td><button id = 'bsub'>-</button></td>
+      <td><button onClick={()=>{console.log(4)}} >4</button></td>
+      <td><button onClick={()=>{console.log(5)}} >5</button></td>
+      <td><button onClick={()=>{console.log(6)}} >6</button></td>
+      <td><button onClick={()=>{console.log('-')}} >-</button></td>
     </tr>
     <tr>
-      <td><button id = 'b7'>7</button></td>
-      <td><button id = 'b8'>8</button></td>
-      <td><button id = 'b9'>9</button></td>
-      <td><button id = 'bplus'>*</button></td>
+      <td><button onClick={()=>{console.log(7)}} >7</button></td>
+      <td><button onClick={()=>{console.log(8)}} >8</button></td>
+      <td><button onClick={()=>{console.log(9)}} >9</button></td>
+      <td><button onClick={()=>{console.log('*')}} >*</button></td>
     </tr>
     <tr>
-      <td  id = 'b0'><button>0</button></td>
-      <td  id = 'bdiv'><button>/</button></td>          
+      <td  id = 'b0'><button onClick={()=>{console.log(0)}}>0</button></td>
+      <td  id = 'bdiv'><button onClick={()=>{console.log('/')}}>/</button></td>          
     </tr>
     <tr>
-      <td ><button class="ac" id = 'bclean'>AC</button></td>
-      <td><button id = 'bcoma'>.</button></td>
-      <td><button id= 'bequal'>=</button></td>          
+      <td ><button onClick={()=>{console.log('AC')}} className="AC">AC</button></td>
+      <td><button onClick={()=>{console.log('.')}}>.</button></td>
+      <td><button onClick={()=>{console.log('=')}}>=</button></td>          
     </tr>
   </table>
   );
